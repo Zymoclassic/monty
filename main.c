3463da17cacce7409e5ea54be35b53485f02fec1
zymoclassic <awopejumoses@gmail.com>
2023-06-22 14:42:48 -0700

    main.c

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include "monty.h"
+#include <stdio.h>
+#define _GNU_SOURCE
+#include <stdlib.h>
 
 bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
- * main - monty code interpreter
- * @argc: number of arguments
- * @argv: monty file location
- * Return: 0 on success
- */
+* main - function for monty code interpreter
+* @argc: argument count
+* @argv: argument value
+*
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
 	char *content;
