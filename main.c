0fb3a9b5c07541a0d4c007df0aea7ed6027b28e5
zymoclassic <awopejumoses@gmail.com>
2023-06-25 01:01:39 -0700

    errors fixed

--- main.c
+++ main.c
@@ -1,29 +1,53 @@
 #include "monty.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
-char **op_toks = NULL;
+globales_t globalvar = {NULL, NULL, NULL};
 
 /**
- * main - the entry point for Monty Interp
- *
- * @argc: the count of arguments passed to the program
+ * main - entry point for the CLI program
+ * @argc: count of arguments passed to the program
  * @argv: pointer to an array of char pointers to arguments
- *
- * Return: (EXIT_SUCCESS) on success (EXIT_FAILURE) on error
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
+
 int main(int argc, char **argv)
 {
-	FILE *script_fd = NULL;
-	int exit_code = EXIT_SUCCESS;
+	char *token = NULL;
+	size_t line_buf_size = 0;
+	int line_number = 0, flag = 0, flag2 = 0;
+	ssize_t line_size;
+	stack_t *stack = NULL;
 
 	if (argc != 2)
-		return (usage_error());
-	script_fd = fopen(argv[1], "r");
-	if (script_fd == NULL)
-		return (f_open_error(argv[1]));
-	exit_code = run_monty(script_fd);
-	fclose(script_fd);
-	return (exit_code);
+		stderr_usage();
+	globalvar.fd = fopen(argv[1], "r");
+	if (globalvar.fd == NULL)
+		stderr_fopen(argv[1]);
+	line_size = getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
+	if (globalvar.line_buf[0] == '#')
+		line_size = getline(&globalvar.line_buf, &line_buf_size, globalvar.fd);
+	while (line_size >= 0)
+	{flag = 0;
+		flag2 = 0;
+		line_number++;
+		token = strtok(globalvar.line_buf, DELIM);
+		globalvar.token2 = strtok(NULL, DELIM);
+		if (token == NULL)
+		{flag2 = 1;
+			nop(&stack, line_number); }
+		if (flag2 == 0)
+		{
+			if (token[0] == '#')
+			{
+				line_size = getline(&globalvar.line_buf,
+						    &line_buf_size, globalvar.fd);
+				flag = 1; }}
+		if (flag == 0)
+		{get_builtin(token, &stack, line_number);
+			line_size = getline(&globalvar.line_buf, &line_buf_size,
+					    globalvar.fd); }}
+	free_dlistint(stack);
+	free(globalvar.line_buf);
+	globalvar.line_buf = NULL;
+	fclose(globalvar.fd);
+	return (EXIT_SUCCESS);
 }
--- stacks.c
+++ stacks.c
@@ -1,66 +1,52 @@
 #include "monty.h"
-#include <string.h>
-
-void free_stack(stack_t **stack);
-int init_stack(stack_t **stack);
-int check_mode(stack_t *stack);
 
 /**
- * free_stack - Frees a stack_t stack.
- * @stack: A pointer to the top (stack) or
- *         bottom (queue) of a stack_t.
+ * create_node_stack - create a node stack.
+ * @stack: A pointer to top and bottom stack.
+ * Return: EXIT SUCCESS
  */
-void free_stack(stack_t **stack)
+int create_node_stack(stack_t **stack)
 {
-	stack_t *tmp = *stack;
+	stack_t *p;
 
-	while (*stack)
-	{
-		tmp = (*stack)->next;
-		free(*stack);
-		*stack = tmp;
-	}
-}
+	p = malloc(sizeof(stack_t));
+	if (p == NULL)
+		stderr_malloc();
+	p->n = STACK;
+	p->prev = NULL;
+	p->next = NULL;
 
-/**
- * init_stack - Initializes a stack_t stack with beginning
- *              stack and ending queue nodes.
- * @stack: A pointer to an unitialized stack_t stack.
- *
- * Return: If an error occurs - EXIT_FAILURE.
- *         Otherwise - EXIT_SUCCESS.
- */
-int init_stack(stack_t **stack)
-{
-	stack_t *s;
-
-	s = malloc(sizeof(stack_t));
-	if (s == NULL)
-		return (malloc_error());
-
-	s->n = STACK;
-	s->prev = NULL;
-	s->next = NULL;
-
-	*stack = s;
+	*stack = p;
 
 	return (EXIT_SUCCESS);
 }
-
 /**
- * check_mode - Checks if a stack_t linked list is in stack or queue mode.
- * @stack: A pointer to the top (stack) or bottom (queue)
- *         of a stack_t linked list.
- *
- * Return: If the stack_t is in stack mode - STACK (0).
- *         If the stack_t is in queue mode - QUEUE (1).
- *         Otherwise - 2.
+ * free_node_stack - Frees a stack_t stack.
+ * @stack: A pointer to top and bottom stack.
  */
-int check_mode(stack_t *stack)
+void free_node_stack(stack_t **stack)
 {
+	stack_t *temp_node = *stack;
+
+	while (*stack)
+	{
+		temp_node = (*stack)->next;
+		free(*stack);
+		*stack = temp_node;
+	}
+}
+/**
+ * check_opcode - checks if STACK or QUEUE.
+ * @stack: A pointer to top and bottom stack.
+ * Return: The value of Stack or Queue
+ */
+int check_opcode(stack_t *stack)
+{
+	int ret_val = 2;
+
 	if (stack->n == STACK)
 		return (STACK);
 	else if (stack->n == QUEUE)
 		return (QUEUE);
-	return (2);
+	return (ret_val);
 }
